f250a555a351619b49ab72a71cd72c8c8cb3d5bf
Thomas Swatland <thomas@indicosys.com>
2021-02-25 08:20:39 +0100

    Begin refactoring from OOP to POP

--- EvidenceRoom/Camera.swift
+++ EvidenceRoom/Camera.swift
@@ -11,19 +11,32 @@ struct Room {
         
         var cams = [Camera]()
         apolloRoom.cameras?.forEach({
-            let camera = Camera(apolloCam: $0)
-            cams.append(camera)
+                        
+            switch $0.__typename {
+            case "Axis":
+                let camera = Axis(apolloCam: $0)
+                cams.append(camera)
+            default:
+                break
+            }
         })
         self.cameras = cams
     }
 }
 
-class Camera {
+protocol Camera {
+    var id: String { get }
+    var name: String { get }
+    var address: String { get }
+    var url: URL { get }
+    var serverURL: URL { get }
+    var streamURL: URL { get }
     
-    let id: String
-    let name: String
-    let address: String
-    
+    func startRecording(completion: @escaping (Result<Bool, Error>) -> ())
+    func stopRecording(completion: @escaping (Result<RecordingInfo, Error>) -> ())
+}
+
+extension Camera {
     var url: URL {
         let ip = address.split(separator: ":").first!
         return URL(string: "http://\(ip)")!
@@ -33,25 +46,114 @@ class Camera {
         return URL(string: "http://\(address)")!
     }
     
-    init(apolloCam: GetRoomsQuery.Data.Room.Camera) {
-        self.id = apolloCam.id
-        self.name = apolloCam.name
-        self.address = apolloCam.address
-    }
-    
     var streamURL: URL {
         return serverURL.appendingPathComponent("feed")
     }
 }
 
-class AxisCamera: Camera {
-    
-    enum Request: String {
-        case start = "/record/start"
-        case stop  = "/record/stop"
+extension Camera {
+    func startRecording(completion: @escaping (Result<Bool, Error>) -> ()) {
+        var request = URLRequest(url: serverURL.appendingPathComponent("record/start"))
+        request.httpMethod = "POST"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let err = error {
+                completion(.failure(err))
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                switch response.statusCode {
+                case 200:
+                    completion(.success(true))
+                case 208:
+                    completion(.success(false))
+                case 404:
+                    completion(.failure(CameraNetworkingClientError.notFound404))
+                default:
+                    completion(.failure(CameraNetworkingClientError.unknownError))
+                }
+            }
+        }.resume()
     }
     
-    var stream: URL {
-        return URL(string: "rtsp://\(address)/axis-media/media.amp")!
+    func stopRecording(completion: @escaping (Result<RecordingInfo, Error>) -> ()) {
+        var request = URLRequest(url: serverURL.appendingPathComponent("record/stop"))
+        request.httpMethod = "POST"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let err = error {
+                completion(.failure(err))
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                switch response.statusCode {
+                case 200:
+                    if let data = data {
+                        do {
+                            let recordingInfo = try JSONDecoder().decode(RecordingInfo.self, from: data)
+                            completion(.success(recordingInfo))
+                        } catch {
+                            completion(.failure(error))
+                        }
+                    }
+                case 208:
+                    print("Already sent")
+                case 400...500:
+                    print("Error:", response.statusCode)
+                default:
+                    break
+                }
+            }
+        }.resume()
     }
 }
+
+struct Axis: Camera, Uploader {
+    var id: String
+    var name: String
+    var address: String
+    
+    init(apolloCam: GetRoomsQuery.Data.Room.Camera) {
+        self.id = apolloCam.id
+        self.name = apolloCam.name
+        self.address = apolloCam.address
+    }
+}
+
+//class Camera {
+//
+//    let id: String
+//    let name: String
+//    let address: String
+//
+//    var url: URL {
+//        let ip = address.split(separator: ":").first!
+//        return URL(string: "http://\(ip)")!
+//    }
+//
+//    var serverURL: URL {
+//        return URL(string: "http://\(address)")!
+//    }
+//
+//    init(apolloCam: GetRoomsQuery.Data.Room.Camera) {
+//        self.id = apolloCam.id
+//        self.name = apolloCam.name
+//        self.address = apolloCam.address
+//    }
+//
+//    var streamURL: URL {
+//        return serverURL.appendingPathComponent("feed")
+//    }
+//}
+//
+//class AxisCamera: Camera {
+//
+//    enum Request: String {
+//        case start = "/record/start"
+//        case stop  = "/record/stop"
+//    }
+//
+//    var stream: URL {
+//        return URL(string: "rtsp://\(address)/axis-media/media.amp")!
+//    }
+//}
---
+++ EvidenceRoom/Uploader.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+protocol Uploader {
+    func upload(file: File)
+}
+
+extension Uploader where Self: Camera {
+    func upload(file: File) {
+        var request = URLRequest(url: serverURL.appendingPathComponent("upload"))
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = [
+            "Content-Type" : "application/json-header"
+        ]
+
+        let fileData = try! JSONEncoder().encode(file)
+        request.httpBody = fileData
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let err = error  {
+                print(err.localizedDescription)
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                if response.statusCode == 200 {
+                    print(String(data: data!, encoding: .utf8))
+                } else {
+                    print("ERROR! \(response.statusCode)")
+                }
+            }
+        }.resume()
+    }
+}
