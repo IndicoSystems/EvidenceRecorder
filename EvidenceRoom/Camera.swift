e9241e070911e8089b2101efb22b7b01c3a7afbb
Thomas Swatland <thomas@indicosys.com>
2021-03-04 14:28:15 +0100

    Add upload function to Camera

--- EvidenceRoom/Camera.swift
+++ EvidenceRoom/Camera.swift
@@ -77,7 +77,32 @@ extension Camera {
                 case 400...500:
                     print("Error:", response.statusCode)
                 default:
-                    break
+                    print("Error:", response.statusCode)
+                }
+            }
+        }.resume()
+    }
+    
+    func upload(file: File) {
+        var request = URLRequest(url: serverURL.appendingPathComponent("file"))
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = [
+            "Content-Type" : "application/json-header"
+        ]
+
+        let fileData = try! JSONEncoder().encode(file)
+        request.httpBody = fileData
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let err = error  {
+                print(err.localizedDescription)
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                if response.statusCode == 200 {
+                    print(String(data: data!, encoding: .utf8) ?? "No data")
+                } else {
+                    print("ERROR! \(response.statusCode)")
                 }
             }
         }.resume()
