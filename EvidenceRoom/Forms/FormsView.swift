b3366730dad9c1335c914cd8b49d91dfb0d27fc6
Thomas Swatland <thomas@indicosys.com>
2021-03-04 14:28:02 +0100

    Add navigation to FormsView

--- EvidenceRoom/Forms/FormsView.swift
+++ EvidenceRoom/Forms/FormsView.swift
@@ -4,30 +4,23 @@ struct FormsView: View {
     
     @ObservedObject private var cloudClient = CloudClient.shared
     
+    @Binding var activeForm: ERForm?
+    
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        
-        List {
-            ForEach(cloudClient.forms, id: \.self) { form in
-                Button {
-                    print(form)
-                } label: {
-                    Text(form)
+        NavigationView {
+            List {
+                ForEach(cloudClient.forms, id: \.form.form.id) { form in
+                    Text(form.title)
+                        .font(.title2)
+                        .padding()
+                        .onTapGesture {
+                            activeForm = form
+                            presentationMode.wrappedValue.dismiss()
+                        }
                 }
-            }
+            }.navigationTitle(Text("Forms"))
         }
-        .navigationTitle("Forms")
-        
-        
-//        GeometryReader { gr in
-//            Form {
-//                ForEach(cloudClient.forms, id: \.self) { form in
-//                    Text(form)
-//                        .font(.custom("Roboto-Regular", size: 32))
-//                        .foregroundColor(.text)
-//                        .listRowBackground(Color.clear)
-//                        .padding()
-//                }
-//            }.frame(width: gr.size.width * 0.5)
-//        }
     }
 }
