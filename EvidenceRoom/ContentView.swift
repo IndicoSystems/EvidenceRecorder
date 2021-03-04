39a25a21edbb69380f50621db58e285f01667147
Thomas Swatland <thomas@indicosys.com>
2021-03-04 14:20:21 +0100

    Clean up ContentView

--- EvidenceRoom/ContentView.swift
+++ EvidenceRoom/ContentView.swift
@@ -14,16 +14,16 @@ struct ContentView: View {
         
         NavigationView {
             VStack(spacing: 16) {
-                NavigationLink(destination: MainView()) {
-                    Text("Main")
-                }
-                Divider()
-                    .padding()
-                NavigationLink(destination: FormsView()) {
-                    Text("Forms")
-                }
-                Divider()
-                    .padding()
+//                NavigationLink(destination: MainView()) {
+//                    Text("Main")
+//                }
+//                Divider()
+//                    .padding()
+//                NavigationLink(destination: FormsView()) {
+//                    Text("Forms")
+//                }
+//                Divider()
+//                    .padding()
                 NavigationLink(destination: RoomsView()) {
                     Text("Rooms")
                 }
@@ -42,38 +42,6 @@ struct ContentView: View {
                 }
             )
         }.fullScreenCover(isPresented: $authClient.isSignedOut, content: LoginView.init)
-        
-        
-//        ZStack {
-//            Color.background
-//            HStack(spacing: 0) {
-////                NavBarView()
-////                Spacer()
-//                VStack(alignment: .leading) {
-//                    Text("Dashboard")
-//                        .font(.largeTitle)
-//                        .fontWeight(.bold)
-//                        .foregroundColor(.text)
-//                        .padding()
-////                    MainView()
-////                        .padding()
-//                    RoomsView()
-////                    FormsView()
-////                        .background(Color.card)
-////                        .padding()
-////                        .fullScreenCover(isPresented: $authClient.isSignedOut, content: LoginView.init)
-//                }
-////                Spacer()
-//            }
-//            Button {
-//                authClient.signOut()
-//            } label: {
-//                Image(systemName: "icloud.and.arrow.down.fill")
-//                    .resizable()
-//                    .frame(width: 32, height: 32)
-//            }
-//            .padding()
-//        }
     }
 }
 
