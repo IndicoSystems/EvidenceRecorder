a7f2ca1e6a4116e7c96869cab9581dc0c1ad8ee0
Thomas Swatland <thomas@indicosys.com>
2021-03-04 14:27:02 +0100

    Use grid for RoomView

--- EvidenceRoom/Rooms/RoomView.swift
+++ EvidenceRoom/Rooms/RoomView.swift
@@ -4,18 +4,35 @@ struct RoomView: View {
     
     let room: Room
     
+    let columns: [GridItem] = [
+//        GridItem(.adaptive(minimum: 500, maximum: .infinity), alignment: .center)
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        VStack {
-            ForEach(room.cameras, id: \.id) { camera in
-                CameraView(viewModel: CameraViewModel(camera: camera))
+//        VStack {
+//            ForEach(room.cameras, id: \.id) { camera in
+//                CameraView(viewModel: CameraViewModel(camera: camera))
+//            }
+//        }
+//        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(room.cameras, id: \.id) { camera in
+                    
+                    CameraView(viewModel: CameraViewModel(camera: camera))
+                        .background(Color.gray.opacity(0.2))
+                }
             }
+            .navigationTitle(room.name)
+            .padding()
         }
-    }
+//    }
 }
 
-struct RoomView_Previews: PreviewProvider {
-    
-    static var previews: some View {
-        RoomView(room: Room(apolloRoom: GetRoomsQuery.Data.Room(id: "1234", name: "Mock")))
-    }
-}
+//struct RoomView_Previews: PreviewProvider {
+//
+//    static var previews: some View {
+//        RoomView(room: Room(apolloRoom: GetRoomsQuery.Data.Room(id: "1234", name: "Mock")))
+//    }
+//}
