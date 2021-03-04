d99682232411d6e352eccab589585d82e97af347
Thomas Swatland <thomas@indicosys.com>
2021-03-04 14:25:12 +0100

    Add navigation to CasesView

--- EvidenceRoom/Cases/CasesView.swift
+++ EvidenceRoom/Cases/CasesView.swift
@@ -1,10 +1,3 @@
-//
-//  CasesView.swift
-//  EvidenceRoom
-//
-//  Created by Thomas Swatland on 10/02/2021.
-//
-
 import SwiftUI
 
 struct CasesView: View {
@@ -14,14 +7,21 @@ struct CasesView: View {
     var body: some View {
         List {
             ForEach(cloudClient.cases, id: \.id) { c in
-                Text(c.name)
+                NavigationLink(
+                    destination: CaseView().environmentObject(c),
+                    label: {
+                        Text(c.name)
+                            .font(.title)
+                            .padding()
+                    })
             }
         }
         .onAppear {
-            cloudClient.getCases()
+            if cloudClient.cases.isEmpty {
+                cloudClient.getCases()
+            }
         }
         .navigationTitle("Cases")
-        
     }
 }
 
