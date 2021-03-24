c92e47040564a905bfbd1f71e996e87853fca2aa
Thomas Swatland <thomas@indicosys.com>
2021-03-24 10:05:58 +0100

    Add TaskField model

--- EvidenceRoom/Task/TaskField.swift
+++ EvidenceRoom/Task/TaskField.swift
@@ -1,8 +1,28 @@
-//
-//  TaskField.swift
-//  EvidenceRoom
-//
-//  Created by Thomas Swatland on 22/03/2021.
-//
-
 import Foundation
+
+struct TaskField: Codable {
+    let id: String
+    let title: String
+    let placeholder: String
+    let description: String
+    let required: String
+    let type: String
+    let answer: String?
+    let exhibits: [Exhibit]
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case title = "title"
+        case placeholder = "placeholder"
+        case description = "description"
+        case required = "required"
+        case type = "type"
+        case answer = "answer"
+        case exhibits = "exhibits"
+    }
+}
+
+struct Language: Codable {
+    let key: String
+    let value: String
+}
