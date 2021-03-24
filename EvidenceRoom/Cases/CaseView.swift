18077bf95f98922abd9b60439b0daed12371c302
Thomas Swatland <thomas@indicosys.com>
2021-03-24 10:03:12 +0100

    Populate CasesView and CaseView

--- EvidenceRoom/Cases/CaseView.swift
+++ EvidenceRoom/Cases/CaseView.swift
@@ -2,16 +2,34 @@ import SwiftUI
 
 struct CaseView: View {
     
-//    @ObservedObject var `case`: ERCase
-    
     let project: Project
     
     @State private var isShowingFormsView = false
     
-    @State private var activeReport: ERForm?
+    @ObservedObject var cloudClient = CloudClient.shared
     
     var body: some View {
-        Text(project.name)
+        VStack {
+            if cloudClient.tasks.count <= 0 {
+                ProgressView()
+            } else {
+                ForEach(cloudClient.tasks, id: \.id) { task in
+                    NavigationLink(destination: TaskView(task: task)) {
+                        Text(task.name)
+                            .padding()
+                            .background(Color.orange)
+                            .cornerRadius(8)
+                    }
+                }
+            }
+        }
+//        List(cloudClient.tasks, id: \.id) { task in
+//            Text(task.name)
+//        }
+        .onAppear {
+            cloudClient.getTasks(inProjectWithID: project.id)
+        }
+        .navigationTitle(Text(project.name))
     }
 }
 
--- EvidenceRoom/Cases/CasesView.swift
+++ EvidenceRoom/Cases/CasesView.swift
@@ -3,24 +3,47 @@ import SwiftUI
 struct CasesView: View {
     
     @ObservedObject var cloudClient = CloudClient.shared
+    @State private var permission = Permission.all
     
     var body: some View {
-        if cloudClient.projects.count <= 0 {
-            ProgressView()
-                .navigationTitle("Cases")
-        } else {
-            List {
-                ForEach(cloudClient.projects, id: \.id) { project in
-                    NavigationLink(
-                        destination: CaseView(project: project),
-                        label: {
-                            Text(project.name)
-                                .font(.title)
-                                .padding()
-                        })
+        
+        VStack {
+            Picker("Permission", selection: $permission) {
+                ForEach(Permission.allCases, id: \.self) {
+                    Text($0.rawValue.uppercased())
                 }
-            }.navigationTitle("Cases")
-        }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            
+            if cloudClient.projects.count <= 0 {
+                Text("No projects found")
+                    .foregroundColor(.gray)
+                    .fontWeight(.bold)
+            } else {
+                List {
+                    Section {
+                        switch permission {
+                        case .all:
+                            ForEach(cloudClient.projects, id: \.id) { project in
+                                NavigationLink(
+                                    destination: CaseView(project: project),
+                                    label: {
+                                        ProjectCellView(project: project)
+                                    })
+                            }
+                        default:
+                            ForEach(cloudClient.projects.filter({ $0.permission == permission }), id: \.id) { project in
+                                NavigationLink(
+                                    destination: CaseView(project: project),
+                                    label: {
+                                        ProjectCellView(project: project)
+                                    })
+                            }
+                        }
+                    }
+                }
+            }
+        }.navigationTitle("Cases")
     }
 }
 
