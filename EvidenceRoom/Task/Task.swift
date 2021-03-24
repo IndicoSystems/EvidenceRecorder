832982d34009f07db27f8595d28c8d66cb0c40d0
Thomas Swatland <thomas@indicosys.com>
2021-03-24 10:06:09 +0100

    Add Task model

--- EvidenceRoom/Task/Task.swift
+++ EvidenceRoom/Task/Task.swift
@@ -1,8 +1,25 @@
-//
-//  Task.swift
-//  EvidenceRoom
-//
-//  Created by Thomas Swatland on 18/03/2021.
-//
-
 import Foundation
+
+struct Task: Codable {
+    let id: String
+    let name: String
+    let projectId: String?
+    let projectName: String?
+    let dueAt: String?
+    let fields: [TaskField]?
+    
+    var date: Date {
+        let dateFormatter = DateFormatter()
+        guard let dueAt = dueAt else { return Date.distantFuture }
+        return dateFormatter.date(from: dueAt) ?? Date.distantPast
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case projectId = "project_id"
+        case projectName = "project_name"
+        case dueAt = "due_at"
+        case fields = "fields"
+    }
+}
