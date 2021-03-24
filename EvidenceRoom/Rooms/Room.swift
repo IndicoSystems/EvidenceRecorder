ef9d68de7acd5700f91943c63c1ea19fd5c65912
Thomas Swatland <thomas@indicosys.com>
2021-03-24 10:06:26 +0100

    Add function to start all cameras in Room object

--- EvidenceRoom/Rooms/Room.swift
+++ EvidenceRoom/Rooms/Room.swift
@@ -3,4 +3,25 @@ import Foundation
 struct Room: Codable {
     let id: String
     let name: String
+    
+    var cameras: [Camera] {
+        CloudClient.shared.cameras.filter({ id == $0.roomId })
+    }
+    
+    func startAllCameras() {
+        for camera in cameras {
+            camera.startRecording { result in
+                switch result {
+                case .success(let success):
+                    if success {
+                        print("\(camera.name) started")
+                    } else {
+                        print("\(camera.name) is already recording")
+                    }
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
 }
