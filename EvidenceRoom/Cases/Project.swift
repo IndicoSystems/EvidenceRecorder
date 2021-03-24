f6d6f95ee23597297023df5844c177e77ffc0c5c
Thomas Swatland <thomas@indicosys.com>
2021-03-24 10:03:33 +0100

    Add Permissions enum to Project

--- EvidenceRoom/Cases/Project.swift
+++ EvidenceRoom/Cases/Project.swift
@@ -1,6 +1,14 @@
 import Foundation
 
+enum Permission: String, CaseIterable, Codable {
+    case all
+    case read
+    case write
+    case visible
+}
+
 struct Project: Codable {
     let id: String
     let name: String
+    let permission: Permission
 }
