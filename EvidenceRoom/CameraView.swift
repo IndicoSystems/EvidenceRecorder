9462ee51cb1c5734eda8e6f46cd4c7ebe47e2bb4
Thomas Swatland <thomas@indicosys.com>
2021-04-09 14:11:45 +0200

    Clean up and refactor CameraView and RoomView

--- EvidenceRoom/CameraView.swift
+++ EvidenceRoom/CameraView.swift
@@ -1,45 +1,17 @@
 import SwiftUI
-import SwiftKeychainWrapper
-
-class CameraViewModel: ObservableObject {
-    
-    var camera: Camera
-    
-    @Published var isRecording = false
-    @Published var isPreviewing = false
-    
-    init(camera: Camera) {
-        self.camera = camera
-    }
-    
-    func stopRecording() {
-        camera.stopRecording() { [unowned self] result in
-            switch result {
-            case .success(let recordingResponse):
-//                self.getUploadURL(recordingInfo: recordingResponse)
-                
-                DispatchQueue.main.async {
-                    self.isRecording = false
-                }
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-}
 
 struct CameraView: View {
     
-    @ObservedObject var viewModel: CameraViewModel
+    let camera: Camera
     
     @State private var isRecording = false
     
     var body: some View {
         VStack {
-            Text(viewModel.camera.name)
+            Text(camera.name)
                 .font(.custom("Roboto-Regular", size: 20))
                 .foregroundColor(.text)
-            WebView(url: viewModel.camera.streamURL!.string)
+            WebView(url: camera.streamURL!.string)
                 .frame(width: 400, height: 400 / (16/9))
             Button {
                 isRecording.toggle()
--- EvidenceRoom/Rooms/RoomView.swift
+++ EvidenceRoom/Rooms/RoomView.swift
@@ -4,104 +4,21 @@ struct RoomView: View {
     
     let room: Room
     
-    @State private var cameras = [Camera]()
-    
-//    var cameras: [Camera] {
-//        CloudClient.shared.cameras.filter({$0.roomId == room.id})
-//    }
-    
     let columns: [GridItem] = [
-//        GridItem(.adaptive(minimum: 500, maximum: .infinity), alignment: .center)
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
     
-    @State private var allCamerasRecording = false
-    
-    @State private var cameraViewModels = [CameraViewModel]()
-    
     var body: some View {
         ScrollView {
             LazyVGrid(columns: columns) {
-                ForEach(cameras, id: \.id) { camera in
-
-                    CameraView(viewModel: CameraViewModel(camera: camera))
+                ForEach(room.cameras, id: \.id) { camera in
+                    CameraView(camera: camera)
                         .background(Color.gray.opacity(0.2))
                 }
             }
             .navigationTitle(room.name)
-            .navigationBarItems(trailing: Button(allCamerasRecording ? "Stop all" : "Start all") {
-
-//                allCamerasRecording ? stopAllCameras() : startAllCameras()
-
-                toggleAllCameras()
-            })
             .padding()
-        }.onAppear {
-            cameras = CloudClient.shared.cameras.filter({$0.roomId == room.id})
         }
     }
-    
-    func toggleAllCameras() {
-        cameraViewModels.removeAll()
-        
-//        for (i, cam) in room.cameras.enumerated() {
-//
-//            let camViewModel = CameraViewModel(camera: cam)
-//            cameraViewModels.append(camViewModel)
-//            cameraViewModels[i].isRecording ? camViewModel.stopRecording() : camViewModel.startRecording()
-//        }
-        
-        allCamerasRecording.toggle()
-    }
-    
-    func startAllCameras() {
-//        let uploadingCameras = room.cameras.filter({$0 is Uploader})
-        
-//        for cam in room.cameras {
-//
-//            let camViewModel = CameraViewModel(camera: cam)
-//            camViewModel.startRecording()
-//
-////            cam.startRecording() { result in
-////                switch result {
-////                case .success(let success):
-////                    if success {
-////                        print("Success!")
-////                        allCamerasRecording = true
-////                    }
-////                case .failure(let error):
-////                    print(error.localizedDescription)
-////                }
-////            }
-//
-//        }
-    }
-    
-    func stopAllCameras() {
-//        let uploadingCameras = room.cameras.filter({$0 is Uploader})
-        
-//        for cam in room.cameras {
-//            
-//            let camViewModel = CameraViewModel(camera: cam)
-//            camViewModel.stopRecording()
-////            cam.stopRecording() { result in
-////                switch result {
-////                case .success(let recordingResponse):
-////                    cam.getUploadURL(recordingInfo: recordingResponse)
-////                    allCamerasRecording = false
-////                case .failure(let error):
-////                    print(error.localizedDescription)
-////                }
-////            }
-//        }
-    }
-//    }
 }
-
-//struct RoomView_Previews: PreviewProvider {
-//
-//    static var previews: some View {
-//        RoomView(room: Room(apolloRoom: GetRoomsQuery.Data.Room(id: "1234", name: "Mock")))
-//    }
-//}
