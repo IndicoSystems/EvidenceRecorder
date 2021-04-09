609ade6e6004816eb2a8b682c48b20dcaee2fdf5
Thomas Swatland <thomas@indicosys.com>
2021-04-09 09:46:41 +0200

    Remove unused code from CameraView

--- EvidenceRoom/CameraView.swift
+++ EvidenceRoom/CameraView.swift
@@ -12,25 +12,6 @@ class CameraViewModel: ObservableObject {
         self.camera = camera
     }
     
-//    func startRecording() {
-//        camera.startRecording() { [weak self] result in
-//            guard let self = self else { return }
-//            switch result {
-//            case .success(let success):
-//                if success {
-//                    DispatchQueue.main.async {
-//                        self.isRecording = true
-//                    }
-//                }
-//            case .failure(let error):
-//                DispatchQueue.main.async {
-//                    self.isRecording = false
-//                }
-//                print(error.localizedDescription)
-//            }
-//        }
-//    }
-    
     func stopRecording() {
         camera.stopRecording() { [unowned self] result in
             switch result {
@@ -45,27 +26,6 @@ class CameraViewModel: ObservableObject {
             }
         }
     }
-    
-    func getUploadURL(recordingInfo: RecordingInfo) {
-
-//        DispatchQueue.global().async {
-//            AuthClient.shared.authenticate()
-//
-//            DispatchQueue.main.async {
-//                CloudClient.shared.createFile(with: recordingInfo.id, size: recordingInfo.fileSize) { result in
-//                    switch result {
-//                    case .success(let url):
-//                        let file = File(location: url, id: recordingInfo.id)
-//                        if let axisCam = self.camera as? Axis {
-//                            axisCam.upload(file: file)
-//                        }
-//                    case .failure(let error):
-//                        print(error.localizedDescription)
-//                    }
-//                }
-//            }
-//        }
-    }
 }
 
 struct CameraView: View {
@@ -75,40 +35,20 @@ struct CameraView: View {
     @State private var isRecording = false
     
     var body: some View {
-//        ZStack {
-//                Rectangle()
-//                    .foregroundColor(Color.gray.opacity(0.2))
-//        GeometryReader { gr in
-                VStack {
-//                    GeometryReader { gr in
-                    Text(viewModel.camera.name)
-                        .font(.custom("Roboto-Regular", size: 20))
-                        .foregroundColor(.text)
-//                    if viewModel.isPreviewing || viewModel.isRecording {
-                        WebView(url: viewModel.camera.streamURL!.string)
-                            .frame(width: 400, height: 400 / (16/9))
-//                    } else {
-//                        Image("camera")
-////                            .resizable()
-////                            .scaledToFit()
-//                            .foregroundColor(.text)
-//                    }
-                    Button {
-//                        viewModel.isRecording ? viewModel.stopRecording() : viewModel.startRecording()
-                        isRecording.toggle()
-                    } label: {
-                        Image(isRecording ? "stop" : "record")
-                            .resizable()
-                            .frame(width: 56, height: 56)
-                            .foregroundColor(Color.red)
-                    }
-                    
-//                    Button(viewModel.isPreviewing ? "Hide preview" : "Show preview") {
-//                        viewModel.isPreviewing.toggle()
-//                    }
-//                    .buttonStyle(EvidenceButtonStyle(bgColor: .secondary, clipShape: .capsule))
-                    
-                }.padding()
-//        }
+        VStack {
+            Text(viewModel.camera.name)
+                .font(.custom("Roboto-Regular", size: 20))
+                .foregroundColor(.text)
+            WebView(url: viewModel.camera.streamURL!.string)
+                .frame(width: 400, height: 400 / (16/9))
+            Button {
+                isRecording.toggle()
+            } label: {
+                Image(isRecording ? "stop" : "record")
+                    .resizable()
+                    .frame(width: 56, height: 56)
+                    .foregroundColor(Color.red)
+            }
+        }.padding()
     }
 }
