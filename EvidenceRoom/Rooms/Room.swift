a489708793132d600924b0d0f7c4c2d6438d476d
Thomas Swatland <thomas@indicosys.com>
2021-04-09 11:43:32 +0200

    Remove unused methods from Room model

--- EvidenceRoom/Rooms/Room.swift
+++ EvidenceRoom/Rooms/Room.swift
@@ -7,34 +7,4 @@ struct Room: Codable {
     var cameras: [Camera] {
         CloudClient.shared.cameras.filter({ id == $0.roomId })
     }
-    
-//    func startAllCameras() {
-//        for camera in cameras {
-//            camera.startRecording { result in
-//                switch result {
-//                case .success(let success):
-//                    if success {
-//                        print("\(camera.name) started")
-//                    } else {
-//                        print("\(camera.name) is already recording")
-//                    }
-//                case .failure(let error):
-//                    print(error.localizedDescription)
-//                }
-//            }
-//        }
-//    }
-    
-    func stopAllCameras() {
-        for camera in cameras {
-            camera.stopRecording { result in
-                switch result {
-                case .success(let recordingInfo):
-                    print(recordingInfo.fileSize)
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            }
-        }
-    }
 }
