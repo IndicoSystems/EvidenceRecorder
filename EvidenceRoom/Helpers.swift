3c55034ccc77925bea6a0bf73904a0b1e129c0fb
Thomas Swatland <thomas@indicosys.com>
2021-04-09 09:39:53 +0200

    Add Helpers file

---
+++ EvidenceRoom/Helpers.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+struct HTTPMethod {
+    static let get = "GET"
+    static let post = "POST"
+}
+
+struct CameraCommand {
+    static let startRecording = "record/start"
+    static let stopRecording = "record/stop"
+    static let upload = "file"
+    static let feed = "feed"
+}
