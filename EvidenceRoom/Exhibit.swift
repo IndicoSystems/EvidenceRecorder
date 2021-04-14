df42cf5ffa262a9ee50fafecf8eb001c21b4a9b9
Thomas Swatland <thomas@indicosys.com>
2021-04-14 14:48:09 +0200

    Add startedAt field to Exhibit

--- EvidenceRoom/Exhibit.swift
+++ EvidenceRoom/Exhibit.swift
@@ -2,4 +2,10 @@ import Foundation
 
 struct Exhibit: Codable {
     let id: String
+    let startedAt: Date?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case startedAt = "started_at"
+    }
 }
