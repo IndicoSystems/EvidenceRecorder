1ed63f13eb39f83ac449a8c6d6e48adb4bd944ea
Thomas Swatland <thomas@indicosys.com>
2021-07-05 10:36:14 +0200

    Minor changes to SettingsView

--- EvidenceRoom/SettingsView.swift
+++ EvidenceRoom/SettingsView.swift
@@ -2,23 +2,23 @@ import SwiftUI
 
 struct SettingsView: View {
     
-    private var cloudClient = CloudClient.shared
     @ObservedObject var appState = AppState.shared
     @State private var assignedRoom = "Room"
     
     var body: some View {
         Form {
-            Section {
-                Picker("Room", selection: $assignedRoom) {
+            Section(header: Text("Assigned room")) {
+                Picker(appState.assignedRoom?.name ?? "No assigned room", selection: $assignedRoom) {
                     ForEach(appState.rooms, id: \.id) { room in
                         Text(room.name)
                     }
-                }.onChange(of: assignedRoom) { roomId in
+                }
+                .onChange(of: assignedRoom) { roomId in
                     UserDefaults.standard.setValue(roomId, forKey: "assignedRoomId")
                 }
             }
+            .navigationTitle(Text("Settings"))
         }
-        .navigationTitle(Text("Settings"))
     }
 }
 
