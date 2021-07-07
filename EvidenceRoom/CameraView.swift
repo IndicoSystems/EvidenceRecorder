a15dd2a09dc36958f7254819949df4d1b39b3999
Thomas Swatland <thomas@indicosys.com>
2021-07-07 15:05:26 +0200

    Remove recording button from CameraView

--- EvidenceRoom/CameraView.swift
+++ EvidenceRoom/CameraView.swift
@@ -13,14 +13,6 @@ struct CameraView: View {
                 .foregroundColor(.text)
             WebView(url: camera.streamURL!.string)
                 .frame(width: 400, height: 400 / (16/9))
-            Button {
-                isRecording.toggle()
-            } label: {
-                Image(isRecording ? "stop" : "record")
-                    .resizable()
-                    .frame(width: 56, height: 56)
-                    .foregroundColor(Color.red)
-            }
         }.padding()
     }
 }
