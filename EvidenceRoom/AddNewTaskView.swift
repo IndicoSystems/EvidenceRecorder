eff1d1a6128fe56c1debceb277e5f907d8c18f89
Thomas Swatland <thomas@indicosys.com>
2021-07-07 09:36:27 +0200

    Dismiss AddNewTaskView on completion

--- EvidenceRoom/AddNewTaskView.swift
+++ EvidenceRoom/AddNewTaskView.swift
@@ -2,8 +2,7 @@ import SwiftUI
 
 class AddNewTaskViewModel: ObservableObject {
     
-    func createTask(withName name: String) {
-        print(AppState.shared.tasks)
+    func createTask(withName name: String, completion: @escaping ()->()) {
         
         let titleField = TaskField(id: UUID().uuidString, entityField: nil, title: ["en" : "Title"], placeholder: nil, description: nil, required: true, type: .text, min: nil, max: nil, features: [], answer: name, exhibits: [], subtasks: [], options: [])
         
@@ -13,12 +12,14 @@ class AddNewTaskViewModel: ObservableObject {
 
         CloudClient.shared.submitTask(task: task) { response in
             if response.statusCode >= 200 && response.statusCode < 300 {
+                Logger.log(category: .change, level: .usersMinor, action: "create_task")
                 DispatchQueue.main.async {
                     AppState.shared.tasks.append(task)
+                    completion()
                 }
             }
         } failure: { error in
-            print(error.error)
+            Logger.log(category: .error, level: .techSupport, action: "create_task", details: ["error" : error.error])
         }
 
     }
@@ -28,6 +29,7 @@ struct AddNewTaskView: View {
     
     @State private var taskName: String = ""
     @StateObject var viewModel = AddNewTaskViewModel()
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
         Form {
@@ -39,7 +41,9 @@ struct AddNewTaskView: View {
             
             Button("Create task") {
                 if !taskName.isEmpty {
-                    viewModel.createTask(withName: taskName)
+                    viewModel.createTask(withName: taskName) {
+                        presentationMode.wrappedValue.dismiss()
+                    }
                 }
             }
         }
