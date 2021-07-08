8578642de0f8867d1c46683ff9fcbb061123cc8a
Thomas Swatland <thomas@indicosys.com>
2021-07-08 10:49:30 +0200

    Center no tasks view in TodayView

--- EvidenceRoom/TodayView.swift
+++ EvidenceRoom/TodayView.swift
@@ -7,7 +7,30 @@ struct TodayView: View {
     @State private var isAddNewTaskShowing = false
     
     var body: some View {
-        Form {
+        
+        ZStack {
+            Form {
+                ForEach(appState.tasks.sorted(by: {$0.date < $1.date}), id: \.id) { task in
+                    NavigationLink(destination: TaskView(viewModel: TaskViewModel(task: task))) {
+                        TodayCellView(viewModel: TodayCellViewModel(task: task))
+                    }
+                }
+            }
+            .navigationTitle(Text("Today"))
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                        isAddNewTaskShowing = true
+                                    }, label: {
+                                        Image(systemName: "plus")
+                                            .padding()
+                                            .background(Color.blue)
+                                            .foregroundColor(.white)
+                                            .cornerRadius(8)
+                                    }).opacity(appState.tasks.count <= 0 ? 0 : 1)
+            )
+            .sheet(isPresented: $isAddNewTaskShowing, content: {
+                AddNewTaskView()
+            })
             if appState.tasks.count <= 0 {
                 VStack {
                     Text("No tasks")
@@ -21,43 +44,8 @@ struct TodayView: View {
                             .cornerRadius(8)
                     })
                 }
-            } else {
-                ForEach(appState.tasks.sorted(by: {$0.date < $1.date}), id: \.id) { task in
-                    NavigationLink(destination: TaskView(viewModel: TaskViewModel(task: task))) {
-                        TodayCellView(viewModel: TodayCellViewModel(task: task))
-                    }
-                }
             }
         }
-        .navigationTitle(Text("Today"))
-        .navigationBarItems(trailing:
-                                Button(action: {
-                                    isAddNewTaskShowing = true
-                                }, label: {
-                                    Image(systemName: "plus")
-                                        .padding()
-                                        .background(Color.blue)
-                                        .foregroundColor(.white)
-                                        .cornerRadius(8)
-                                })
-        )
-        .sheet(isPresented: $isAddNewTaskShowing, content: {
-            AddNewTaskView()
-        })
-//        .onAppear {
-//            cloudClient.getTasks { tasks in
-//                DispatchQueue.main.async {
-//                    appState.tasks = tasks
-//                }
-//            } failure: { error in
-//                Logger.log(category: .error, level: .techDebug, action: "get_tasks", details: ["error" : error.error])
-//            }
-//            cloudClient.getDevices { devices in
-//                DispatchQueue.main.async {
-//                    appState.devices = devices
-//                }
-//            }
-//        }
     }
 }
 
