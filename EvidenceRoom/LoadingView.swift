b327ce88c733cda880ede0703603031214ec67a7
Thomas Swatland <thomas@indicosys.com>
2021-07-08 10:50:55 +0200

    Move data loading to LoadingView

--- EvidenceRoom/LoadingView.swift
+++ EvidenceRoom/LoadingView.swift
@@ -1,26 +1,65 @@
 import SwiftUI
 
+class LoadingViewModel: ObservableObject {
+    
+    func loadDataFromServer(completion: @escaping()->()) {
+        let dispatchGroup = DispatchGroup()
+        
+        dispatchGroup.enter()
+        CloudClient.shared.getTasks { tasks in
+            DispatchQueue.main.async {
+                AppState.shared.tasks = tasks
+                dispatchGroup.leave()
+            }
+        } failure: { error in
+            print(error.error)
+        }
+
+        dispatchGroup.enter()
+        CloudClient.shared.getRooms { rooms in
+            DispatchQueue.main.async {
+                AppState.shared.rooms = rooms
+                dispatchGroup.leave()
+            }
+        }
+        
+        dispatchGroup.enter()
+        CloudClient.shared.getDevices { devices in
+            DispatchQueue.main.async {
+                AppState.shared.devices = devices
+                dispatchGroup.leave()
+            }
+        }
+        
+        dispatchGroup.notify(queue: .main) {
+            completion()
+        }
+    }
+}
+
 struct LoadingView: View {
     
-    @Namespace private var animation
-    @State private var isPresented = false
+    @StateObject private var viewModel = LoadingViewModel()
+    @ObservedObject var appState = AppState.shared
     
     @Binding var isFinishedLoading: Bool
     
     var body: some View {
-        
-        VStack {
-            Image("logo")
-                .resizable()
-                .frame(width: 240, height: 240)
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
             VStack {
-                if isFinishedLoading {
-                    Text("Finished")
-                        .matchedGeometryEffect(id: "Test", in: animation)
-                } else {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 240, height: 240)
+                VStack {
                     ProgressView()
                     Text("Loading")
-                        .matchedGeometryEffect(id: "Test", in: animation)
+                }
+            }
+            .onAppear {
+                viewModel.loadDataFromServer {
+                    isFinishedLoading = true
                 }
             }
         }
