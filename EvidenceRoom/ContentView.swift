e3acdb1f94d2b0ea26e92df1ee992bb56dc84051
Thomas Swatland <thomas@indicosys.com>
2021-07-08 10:58:46 +0200

    Clean up ContentView

--- EvidenceRoom/ContentView.swift
+++ EvidenceRoom/ContentView.swift
@@ -1,73 +1,25 @@
 import SwiftUI
 
-class ContentViewModel: ObservableObject {
-    @Published var isFinishedLoading = false
-    
-    init() {
-        loadDataFromServer()
-    }
-    
-    func loadDataFromServer() {
-        let dispatchGroup = DispatchGroup()
-        
-        dispatchGroup.enter()
-        CloudClient.shared.getTasks { tasks in
-            DispatchQueue.main.async {
-                AppState.shared.tasks = tasks
-                dispatchGroup.leave()
-            }
-        } failure: { error in
-            print(error.error)
-        }
-
-        dispatchGroup.enter()
-        CloudClient.shared.getRooms { rooms in
-            DispatchQueue.main.async {
-                AppState.shared.rooms = rooms
-                dispatchGroup.leave()
-            }
-        }
-        
-        dispatchGroup.enter()
-        CloudClient.shared.getDevices { devices in
-            DispatchQueue.main.async {
-                AppState.shared.devices = devices
-                dispatchGroup.leave()
-            }
-        }
-        
-        dispatchGroup.notify(queue: .main) { [weak self] in
-            self?.isFinishedLoading = true
-        }
-    }
-}
-
 struct ContentView: View {
     
     init() {
         UITableView.appearance().backgroundColor = .clear
     }
     
-    private let cloudClient = CloudClient.shared
     @ObservedObject var appState = AppState.shared
-    
-    @StateObject private var viewModel = ContentViewModel()
+    @State private var isFinishedLoading = false
     
     var body: some View {
-        
-        if appState.isSignedOut {
-            LoginView()
-        } else if viewModel.isFinishedLoading {
+        ZStack {
             HomeView()
-                .fullScreenCover(isPresented: $appState.isSignedOut, content: LoginView.init)
-        } else {
-            LoadingView(isFinishedLoading: $viewModel.isFinishedLoading)
+            
+            LoginView()
+                .opacity(CloudClient.shared.token == nil ? 1 : 0)
+            
+            if !isFinishedLoading && !appState.isSignedOut {
+                LoadingView(isFinishedLoading: $isFinishedLoading)
+                    .opacity(isFinishedLoading ? 0 : 1)
+            }
         }
     }
 }
-
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
